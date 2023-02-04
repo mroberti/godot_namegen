d86c10000a3ec2adaa2bd2ae69b9fb82cbf31499
Mario Roberti <mario.roberti@gmail.com>
2023-02-03 17:30:38 -0800

    Busting out
    Got the rules pasted in to the text file from the original of the file.

--- TestBed.gd
+++ TestBed.gd
@@ -20,11 +20,11 @@ func _ready():
 	}
 	# "rules": "Ap %10P-B %50CD %50PD %50Cp %25E"
 	print(RNGTools.pick_weighted(bag))
-	var species = "human male"
+	var species = "tengu female"
 
 
-	# for n in 18:
-	shuffle_stuff(temp_nugget[str(species)])
+	for n in 18:
+		shuffle_stuff(temp_nugget[str(species)])
 
 func shuffle_stuff(temp_nugget):
 	var result = ""
@@ -36,47 +36,34 @@ func shuffle_stuff(temp_nugget):
 	# if roll succeeds, use that rule, otherwise, roll again
 	randomize()
 	while [true]:
-		print("Rules: "+str(the_rules))
 		rule = the_rules[0] # RNGTools.pick(the_rules)
 		if(rule[0]=="$"):
 			# Erase the $ if it's the first char, otherwise splitting string into array won't work. 
-			# print("Cleaning the $")
 			rule.erase(0, 1)
-			print("Rule:"+rule)
+			# print("Rule:"+rule)
 			break
 		elif(rule[0]=="%"):
 			var name_chunks := rule.split("$") as Array
-			# print("Cleaning the $")
+			# Erase the $ if it's the first char, otherwise splitting string into array won't work. 
 			name_chunks[0].erase(0, 1)
-			print("Rule:"+rule)
+			# print("Rule:"+rule)
 			if(percent(int(name_chunks[0]))):
 				rule.erase(0, rule.find ( "$", 1 ))
 				rule.erase(0, 1)
-				print("Rule:"+rule)
+				# print("Rule:"+rule)
 				break
-			else:
-				print("Roll failed, trying again")
-
-
-	print("We're outta the while loop")
 
 	# rule = rule.replace ( "$", "" )
 	var key1 := rule.split("$") as Array
 	# $s$10v$10c$10m$e
 	for key in key1:
-		print("key "+key)
 		if key[0].is_valid_integer():
 			# Quantity = int(Quantity)
 			var mytestnumber = key.substr(0, key.length()-1)
 			var name_part = key.substr(key.length()-1,key.length())
-			print("name_part "+name_part)
-			print("mytestnumber "+mytestnumber)
 			if(percent(int(mytestnumber))):
 				var the_array := temp_nugget[str(name_part)].split(" ") as Array
 				result = result + RNGTools.pick(the_array)
-				print("We have a successfull roll")
-			else:
-				print("Didn't succeed for roll")
 		elif key=="-":
 			result = result + "-"
 		else:

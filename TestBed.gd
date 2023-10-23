b1a4522f10aaecf7034b0043374ceb4c90185492
Mario Roberti <mario.roberti@gmail.com>
2023-10-23 08:54:08 -0700

    Reworking the loading and parsing for rules
    Enjoy

--- TestBed.gd
+++ TestBed.gd
@@ -1,113 +1,111 @@
 extends Node2D
 # https://github.com/LukeMS/lua-namegen/blob/master/data/creatures.cfg
-var name_rules = load_json("res://assets/data/names/creatures.json")
+var rulesText = load_json("res://assets/data/names/rules2.txt")
+var parse_dict = {
+"s":"syllablesStart",
+"m":"syllablesMiddle",
+"e":"syllablesEnd",
+"p":"syllablesPre",
+"P":"syllablesPost",
+"v":"phonemesVocals",
+"c":"phonemesConsonants",
+"A":"customGroupA",
+"B":"customGroupB",
+"N":"customGroupN",
+"O":"customGroupO"
+}
+
+var species = "giant male"
+
+var main_rules = {}
+	
 func load_json(filename):
 	var file = filename
-	var json_as_text = FileAccess.get_file_as_string(file)
-	var data = JSON.parse_string(json_as_text)
-	return data
+	var the_contents = FileAccess.get_file_as_string(file)
+	#var data = JSON.parse_string(json_as_text)
+	return the_contents
 
 func sort_ascending(a, b):
 	if a[0] < b[0]:
 		return true
 	return false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	randomize()
-	make_name("tengu male")
-	make_name("tengu male")
-
-func make_name(the_name_type):
-	var tempSlap = name_rules
-	var name1 = random_name(tempSlap[str(the_name_type)])
-	$output_console.text = $output_console.text + (name1) + "\n"
-	name1 = name1.left(name1.length() - 1)
-	var location1 = random_name(tempSlap[str("map locations")])
-	#var location2 = random_name(tempSlap[str("map locations")])
-	print(name1+"'s "+location1)
-	$output_console.text = $output_console.text + (name1+"'s "+location1)+"\n"
 	
-func random_name(race1):
-	var race = race1
-	for key in race:
-		if(key!="rules"):
-			print("Key: "+key)
-			print(race[key])
-			race[key] = race[key].split(" ") as Array
-			#print(race[key])
+func _ready():
+	# Assuming you have already loaded the provided file data into a variable named "rulesText"
 
-	var result = ""
-	var the_rules := race.rules.split(" ") as Array
-	the_rules.sort()
-	print(the_rules) # Prints [[4, Tomato], [5, Potato], [9, Rice]].
-	for i in range(the_rules.size() - 1, - 1, -1):
-		#print(the_rules[i])
-		if(the_rules[i].contains("%")):
-			the_rules[i].erase(0, 1)
-			var parsedString = the_rules[i].split("$") as Array
-			if(percent(int(parsedString[0]))):
-				for j in range(parsedString.size()):
-					if(j!=0):
-						result = result + RNGTools.pick(race[""+parsedString[j]+""])+" "
-				break
+	# Initialize an empty string to store the filtered data
+	var filtered_data = ""
+
+	# Split the loaded data into lines
+	var lines = rulesText.split("\n")
+	var parentNode = ""
+	# Iterate through the lines
+	for line in lines:
+		# Check if the line does not contain "//" (a comment) and is not blank
+
+		if(!(line.contains("//")) && line != ""):
+			if(line.contains("name \"")):
+				parentNode=line.split("\"")[1]
+				main_rules[parentNode]={}
+				##print(parentNode)
+			if(line.contains("=")):
+				# The key name
+				var key = line.split("=")[0].strip_edges(true,true)
+				##print("Key: "+key)
+				# The value of the key
+				var value = line.split("=")[1].strip_edges(true,true)
+				value = value.replace("\"", "")
+				value = value.replace(",", "")
+				# #print("Value: "+value)
+				main_rules[parentNode][""+key+""] = value.split(" ") as Array
+			# Append the line to the filtered data
+			filtered_data += line + "\n"
+	
+	for i in range(0,100):
+		var pickedRule
+		while true:
+			pickedRule = RNGTools.pick(main_rules[""+species+""]["rules"])
+			print("rule: "+pickedRule)
+			if pickedRule[0]=="%":
+				# OK we have a percentile rule, let's trim up
+				# the string and what not to pass to percent
+				var tempChunks = pickedRule.split("$") as Array
+				var percent = tempChunks[0].right(tempChunks[0].length()-1)
+				#print("Roll D100 "+percent)
+				#print(percent(percent))
+				if(percent(percent)):
+					#print("WIIIIIIIIINNNNNNNNNNN!!!")
+					process_rule(pickedRule)
+					break
 			else:
-				the_rules.remove_at(i)
-		else:
-			#print("remaining rules: "+str(the_rules))
-			var theRule = RNGTools.pick(the_rules)
-			#print("Picking a remaining non % rule: "+theRule)
-			var parsedString = theRule.split("$") as Array
-			#print(parsedString)
-			for j in range(parsedString.size()):
-				#print("j "+str(j))
-				if(parsedString[j] !=""):
-					if(has_letters_and_numbers(parsedString[j])):
-						#print("Has numbers and letters")
-						# Expected format is $s$80e$25e, so the last
-						# letter is the key, but we need to do a percent
-						# check on that remaining number...
-						#print("Pre parsing: "+parsedString[j])
-						var check = int(parsedString[j].erase(parsedString[j].length()-1, parsedString[j].length()))
-						var thekey = parsedString[j].erase(0, parsedString[j].length()-1)
-						#print("Key "+thekey)
-						#print("check "+str(check))
-						if(percent(check)):
-							result = result + RNGTools.pick(race[""+thekey+""])+" "
-					else:
-						#print("Has only letters")
-						#print("parsedString[j] "+parsedString[j])
-						result = result + RNGTools.pick(race[""+parsedString[j]+""])+" "
-					
-	return result
-
+				#print("bort!")
+				process_rule(pickedRule)
+				break
 
 
 func percent(passed_percentile):
-	print("Percent "+str(passed_percentile))
+	passed_percentile = int(passed_percentile)
+	#print("Percent "+str(passed_percentile))
 	var result = false
 	var roll = RNGTools.randi_range(1,100)
-	print("Roll: "+str(roll))
+	#print("Roll: "+str(roll))
 	if(roll <= passed_percentile):
 		result = true
 	return result
 
-func testingSnippets():
-	for i in 10:
-		print(i)
+func process_rule(the_rule):
+	var result = ""
+	print("Received rule: "+the_rule)
+	var rule_chunks = the_rule.split("$")
+	for i in range(0,rule_chunks.size()):
+		if(i>0):
+			# #print("ah? "+rule_chunks[i])
+			# #print(parse_dict[""+rule_chunks[i]+""])
+			result = result + RNGTools.pick(main_rules[""+species+""][""+parse_dict[""+rule_chunks[i]+""]+""])
+			# #print(main_rules["dwarf male"][""+parse_dict[chunk]+""])
+	#print("Result: "+result)
+	add_output(result)
+	return result
 
-func has_letters_and_numbers(your_string):
-	var regex = RegEx.new()
-	regex.compile("[0-9][0-9]+[a-zA-Z]")
-	if regex.search(str(your_string)):
-		return true
-	else:
-		return false
-
-func has_letters(your_string):
-	var regex = RegEx.new()
-	regex.compile("[a-zA-Z]")
-	if regex.search(str(your_string)):
-		return true
-	else:
-		return false
-
+func add_output(theString):
+	$output_console.text = $output_console.text + theString +"\n"
